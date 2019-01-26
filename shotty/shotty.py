c68a5fe320936769b4af5d300d8f6f37f4e28283
Tom Ferguson <tomrferguson@gmail.com>
2019-01-26 09:57:41 -0500

    Fix the name of the list_snapshots function

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -26,7 +26,7 @@ def snapshots():
 
 @snapshots.command('list')
 @click.option('--project', default=None, help="Only snapshots for project(tag Project:<name>)")
-def list_volumes(project):
+def list_snapshot(project):
     "list EC2 snapshots"
 
     instances = filter_instances(project)
